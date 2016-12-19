7435f7e8c7994836ec715686b6b7cd315a86b5bc
zhang <850626925@qq.com>
2016-12-19 23:10:11 +0800

    commit the header and css

--- build/dev-server.js
+++ build/dev-server.js
@@ -16,28 +16,28 @@ var proxyTable = config.dev.proxyTable
 
 var app = express()
 
-// var appData = require('../data.json');
-// var seller = appData.seller;
-// var goods = appData.goods;
-// var ratings appData.ratings;
+var appData = require('../data.json');
+var seller = appData.seller;
+var goods = appData.goods;
+var ratings = appData.ratings;
 
 var apiRoutes = express.Router();
 apiRoutes.get('/seller',function(req,res){
   res.json({
     errno: 0,
-    data:{name:"seven"}
+    data:seller
   });
 });
 apiRoutes.get('/goods',function(req,res){
   res.json({
     errno: 0,
-    data:{}
+    data:goods
   });
 });
 apiRoutes.get('/ratings',function(req,res){
   res.json({
     errno: 0,
-    data:{}    
+    data:ratings   
   });
 });
 
