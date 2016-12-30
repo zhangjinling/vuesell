85bd316071a7846a045b3360cf47c88654aa1124
zhang <850626925@qq.com>
2016-12-30 22:49:29 +0800

    shop cart

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -35,16 +35,21 @@
 				</li>
 			</ul>
 		</div>
+		<shopcart :minPrice="seller.minPrice" :deliveryPrice="seller.deliveryPrice"></shopcart>
 	</div>
 </template>
 <script type="text/javascript">
 	import BScroll from 'better-scroll';
+	import shopcart from 'components/shopcart/shopcart';
 	const ERR_OK = 0;
 	export default{
 		props: {
 			seller: {
 				type: Object
 			}
+		},
+		components: {
+			shopcart
 		},
 		data() {
 			return {
